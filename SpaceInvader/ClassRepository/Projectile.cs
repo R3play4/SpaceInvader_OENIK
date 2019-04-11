a84496930d54c1795ea3ade48643280a92be28b7
ddfdfg <szabo.rozso@gmail.com>
2019-04-11 17:37:24 +0200

    GameItem, UFO, Projectile classes; ClassRepository project

---
+++ SpaceInvader/ClassRepository/Projectile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRepository
+{
+    class Projectile : GameItem
+    {
+        public Projectile(int y, int x) : base(y, x)
+        {
+            this.x = x;
+            this.y = y;
+            this.r = 2;
+        }
+        public override void Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SpaceInvader/ClassRepository/UFO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRepository
+{
+    public class UFO : GameItem
+    {
+        public int Points { get; set; }
+        public UFO(int x, int y, int points) : base(x,y)
+        {
+            this.Points = points;
+        }
+        public override void Move()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        this.x++;
+                    }
+                    else
+                    {
+                        this.x--;
+                    }                    
+                }
+                this.y--;
+            }
+        }
+
+        public Projectile Shoot()
+        {
+            return new Projectile(this.y + this.r, this.x, false);
+        }
+    }
+}
