1112c5baf68a36899dd71246563627e883d9948e
ddfdfg <szabo.rozso@gmail.com>
2019-04-12 12:22:30 +0200

    Restructured ClassRepository project; Implemented GameModel class

---
+++ SpaceInvader/ClassRepository/Model/GameModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRepository
+{
+    public class GameModel
+    {
+        public List<UFO> UFOs { get; set; }
+        public List<Projectile> Projectiles { get; set; }
+        public List<Shield> Shields { get; set; }
+        public Player Player { get; set; }
+        public GameState GameState { get; set; }
+
+        public GameModel(List<UFO> ufos, List<Projectile> projectiles, List<Shield> shields, Player player, GameState gamestate)
+        {
+            this.UFOs = ufos;
+            this.Projectiles = projectiles;
+            this.Shields = shields;
+            this.Player = player;
+            this.GameState = GameState;
+        }
+
+    }
+
+    public enum GameState
+    {
+        Paused,
+        Running,
+        Finished
+    }
+}
