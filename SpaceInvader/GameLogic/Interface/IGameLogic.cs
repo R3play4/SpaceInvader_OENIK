d8e32c79d14da83a9deffe7c4141c9623c60a57a
REPLAY\R3pl4y <nfodor4@gmail.com>
2019-04-13 18:13:37 +0200

    Created IGameLogic.cs

---
+++ SpaceInvader/GameLogic/Interface/IGameLogic.cs
@@ -0,0 +1,69 @@
+﻿// <copyright file="IGameLogic.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace GameLogic.Interface
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using ClassRepository;
+
+    /// <summary>
+    /// GameLogic interface handles the communication with the GameModel layer.
+    /// </summary>
+    public interface IGameLogic
+    {
+        // Interface might be needed
+
+        /// <summary>
+        /// Gets or sets model interface. Communicates with the GameModel layer.
+        /// </summary>
+        GameModel Model { get; set; }
+
+        /// <summary>
+        /// Gets or sets threads that will handle the different GameItems.
+        /// </summary>
+        Thread[] Threads { get; set; }
+
+        /// <summary>
+        /// Handles player movement.
+        /// </summary>
+        /// <param name="direction">true = right, false = left</param>
+        void PlayerMove(bool direction);
+
+        /// <summary>
+        /// Handles Player shooting logic.
+        /// </summary>
+        void PlayerShoot();
+
+        /// <summary>
+        /// Checks if the projectile hit any relevant GameItems
+        /// </summary>
+        /// <param name="projectile">prjectile that is examined</param>
+        void CollisionCheck(Projectile projectile);
+
+        /// <summary>
+        /// Switches GameState
+        /// </summary>
+        void GameStateSwitch(/*Enum GameState*/);
+
+        /// <summary>
+        /// Checks if Game has ended
+        /// </summary>
+        void GameEnd();
+
+        /// <summary>
+        /// Saves the games current state represented by the GameModel;
+        /// </summary>
+        void SaveGame();
+
+        /// <summary>
+        /// Loads a desired state of the GameModel, that was saved earlier.
+        /// </summary>
+        void LoadGame();
+    }
+}
---
+++ SpaceInvader/GameLogic/Logic/GameLogic.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="GameLogic.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace GameLogic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Handles the Game's logic. Implements IGameLogic interface
+    /// </summary>
+    public class GameLogic
+    {
+    }
+}
