47ac383bd42f3341cb1892872cc60442bf2947b6
REPLAY\R3pl4y <nfodor4@gmail.com>
2019-05-05 19:16:46 +0200

    Added Display dll and created Config class.

---
+++ SpaceInvader/Display/Config.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Display
+{
+    public class Config
+    {
+        // Window Config
+        public static double WindowWidth = 700; 
+        public static double WindowHeight = 300;
+        public static int FrameSize = 4;
+
+        public static Brush BackgroundColor= Brushes.Black;
+        public static Brush FrameColor = Brushes.Red;
+
+        //Player ship
+        public static Brush ShipColor = Brushes.White;
+        public static Brush ShipFrameColor = Brushes.Green;
+
+        //UFO
+        public static Brush UFO1Color = Brushes.Orange;
+        public static Brush UFO2Color = Brushes.Yellow;
+        public static Brush UFO3Color = Brushes.Blue;
+    }
+}
