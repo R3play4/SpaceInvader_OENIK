9ea59657d60f3a14fa39cb40dce5a16ae0ecfe91
R3pl4y4 <nfodor4@gmail.com>
2019-05-10 17:46:00 +0200

    Added Image paths to global settings.

--- SpaceInvader/GlobalSettings/Settings.cs
+++ SpaceInvader/GlobalSettings/Settings.cs
@@ -12,29 +12,32 @@ namespace GlobalSettings
         // Window Config
         public static double WindowWidth = 400;
         public static double WindowHeight = 532;
-        public static int FrameSize = 4;
+        public static int FrameSize = 0;
 
         public static Brush BackgroundColor = Brushes.Black;
         public static Brush FrameColor = Brushes.Red;
 
-        //Player ship
+        // Player ship
         public static Brush ShipColor = Brushes.White;
         public static Brush ShipFrameColor = Brushes.Green;
         public const int ShipSize = 15;
 
-        //UFO
+        // UFO
         public static Brush UFO1Color = Brushes.Orange;
         public static Brush UFO2Color = Brushes.Yellow;
         public static Brush UFO3Color = Brushes.LightBlue;
 
-        //Shield
+        // Shield
         public static Brush ShieldColor = Brushes.Green;
         public const int ShieldWidth = 40;
         public const int ShieldHeight = 15;
 
-        //Projectile
+        // Projectile
         public static Brush ProjectileColor = Brushes.Orange;
         public const int ProjectileLength = 6;
         public const int ProjectileWidth = 2;
+
+        // Sprites
+        public static string SpaceShipBackground = "SpaceShip.jpg";
     }
 }
